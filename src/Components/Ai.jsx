39a6bb9f9bb7d78c70941cc81bb43f73185b6d86
EddieHermoza <eddie.ehc04@gmail.com>
2023-12-11 11:03:56 -0500

    aeaeaAI

--- src/Components/Ai.jsx
+++ src/Components/Ai.jsx
@@ -5,7 +5,7 @@ import Stack from 'react-stackai';
 export default function Ai() {
   return (
     <>
-    {/* <Stack project="https://www.stack-ai.com/embed/c64af075-8eee-4b70-a932-06a632c8f084/718b97ab-4282-42fa-92a3-6aeb705516b7/6535facc5607359530e08113" /> */}
+    <Stack project="https://www.stack-ai.com/embed/c64af075-8eee-4b70-a932-06a632c8f084/718b97ab-4282-42fa-92a3-6aeb705516b7/6535facc5607359530e08113" />
   </>
   )
 }
